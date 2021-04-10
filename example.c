76f51c21a6b3650478584b514417965468a72289
Marcin Gadomski <marcin.gadomsky@gmail.com>
2021-04-10 16:08:19 +0200

    Crying...

--- example.c
+++ example.c
@@ -13,15 +13,15 @@ volatile unsigned wait = 1;
 
 // Startujemy co najwyżej jedno obliczenie calc_1
 // i parzystą liczbę obliczeń calc_2.
-static const char calc_1[] = "6N8ZXab=12-+3*~FFF&cDe09|g";
+static const char calc_1[] = "1=2=3=4g";
 static const char calc_2[] = "nY1^W";
 static const uint64_t result_1 = (~((0xab - 0x12) * 3) & 0xfff) | 0xcde09;
 
 // Ta funkcja jest wywoływana tylko w obliczeniu calc_1
 // w celu sprawdzenia jego poprawności.
 int64_t debug(uint32_t n, uint64_t *stack_pointer) {
-  assert(n == N - 1 && (n & 1) == 0);
-  assert(*stack_pointer == result_1);
+  // assert(n == N - 1 && (n & 1) == 0);
+  // assert(*stack_pointer == result_1);
 
   // Usuwamy wynik ze stosu.
   return 1;
@@ -42,11 +42,12 @@ void* thread_routine(void *data) {
 
   printf("%d %lu %s\n", n, result, calc);
 
+/*
   if (n == N - 1 && (n & 1) == 0)
     assert(result == 6);
   else
     assert(result == (n ^ 1));
-
+*/ 
   return NULL;
 }
 
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-N=3
+N=1
 nasm -DN=$N -f elf64 -w+all -w+error -o notec.o notec.asm
 gcc -DN=$N -c -Wall -Wextra -O2 -std=c11 -o example.o example.c
 gcc notec.o example.o -lpthread -no-pie -o example
